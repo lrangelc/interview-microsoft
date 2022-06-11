8b0ebba8a7dc204c77ef2809a08a1c77b5e0c1f4
Luis Rangel Castro <luisrangelc@gmail.com>
2022-06-11 15:00:09 -0600

    fix: files name

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 console.clear();
 
-const A =  [1, 4, 1];
+const A = [0, 1, 0, 1, 1];
 // const A =  [1, 3, 2];
 
 function solution(A) {
   if (A.length > 0) {
-      const uniq = a => [...new Set(a)];
-      const arrUniq = uniq(A);
+    const uniq = (a) => [...new Set(a)];
+    const arrUniq = uniq(A);
 
-      const n = A.length;
-      const res = (n * (n + 1)) / 2;
-      const sum = A.reduce((a, b) => a + b, 0);
-      if (sum === res) {
-          return arrUniq.length === n ? 1 : 0;
-      }
+    const n = A.length;
+    const res = (n * (n + 1)) / 2;
+    const sum = A.reduce((a, b) => a + b, 0);
+    if (sum === res) {
+      return arrUniq.length === n ? 1 : 0;
+    }
   }
   return 0;
 }
