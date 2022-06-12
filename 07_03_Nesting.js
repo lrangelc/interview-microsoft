f146f217efaca81f523dcf90c29097727ee547bb
Luis Rangel Castro <luisrangelc@gmail.com>
2022-06-11 19:22:27 -0600

    feat: Stacks and Queues - Nesting

---
+++ 07_03_Nesting.js
@@ -0,0 +1,35 @@
+console.clear();
+console.log(`Nesting`);
+
+function solution(S) {
+  const balance = {
+      '(': ')'
+  };
+  const arrayNeg = [];
+
+  let valid = true;
+  let index = 0;
+  const myArray = S.split('');
+
+  while (valid && index < myArray.length) {
+      const element = myArray[index];
+      if (balance[element]) {
+          arrayNeg.push(balance[element]);
+      } else {
+          if (arrayNeg.length > 0) {
+              const pop = arrayNeg.pop();
+              if (pop !== element) {
+                  valid = false;
+              }
+          } else {
+              valid = false;
+          }
+      }
+
+      index++;
+  }
+
+  return valid && arrayNeg.length === 0 ? 1 : 0;
+
+}
+
