1afabb3e09ce8158d3cc96fb885ad95e38d9e809
Luis Rangel Castro <luisrangelc@gmail.com>
2022-06-15 22:00:23 -0600

    add merge sort and quicksort

---
+++ sorting_01_merge_sort.js
@@ -0,0 +1,38 @@
+console.clear();
+console.log(`Merge Sort`);
+
+const baseArray = [10, -1, 2, 5, 0, 6, 4, -5];
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+console.log(baseArray);
+
+const newArray = mergeSort(baseArray);
+
+console.log(newArray);
